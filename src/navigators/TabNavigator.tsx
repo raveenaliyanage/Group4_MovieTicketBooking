189695777efdb39010faca485ec9904a41c2dfbd
Raveena Liyanage <144007393+raveenaliyanage@users.noreply.github.com>
2023-11-10 12:31:02 +0530

    create tab navigators

---
+++ src/navigators/TabNavigator.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import HomeScreen from '../screens/HomeScreen';
+import SearchScreen from '../screens/SearchScreen';
+import TicketScreen from '../screens/TicketScreen';
+import UserAccountScreen from '../screens/UserAccountScreen';
+import {COLORS, FONTSIZE, SPACING} from '../theme/theme';
+import CustomIcon from '../components/CustomIcon';
+import {View, StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+    return(
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen}/>
+            <Tab.Screen name="Search" component={SearchScreen}/>
+            <Tab.Screen name="Ticket" component={TicketScreen}/>
+            <Tab.Screen name="User" component={UserAccountScreen}/>
+        </Tab.Navigator>
+    );
+
+
+};
+
+
+
+
+
+
+
+
+
+const styles = StyleSheet.create({
+
+
+});
+
+export default TabNavigator;
