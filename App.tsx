775b8e08597bfaa3c4ed30437e79ff39bd503003
Raveena Liyanage <144007393+raveenaliyanage@users.noreply.github.com>
2023-11-10 12:53:36 +0530

    check tab navigator

--- App.tsx
+++ App.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react';
-import{Text, View, StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabNavigator from './src/navigators/TabNavigator';
+import MovieDetailsScreen from './src/screens/MovieDetailsScreen';
+import SeatBookingScreen from './src/screens/SeatBookingScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App  =()=>{
     return(
-        <View style={styles.container}>
-            <Text>App</Text>
-        </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Tab" component={TabNavigator}/>
+        <Stack.Screen name="MovieDetails" component={MovieDetailsScreen}/>
+        <Stack.Screen name="SeatBooking" component={SeatBookingScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+        
     );
 };
 
 export default App;
 
-const styles=StyleSheet.create({
-    container:{},
-});
+
 
