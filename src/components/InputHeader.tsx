21d0983fb41b4d2d4bc9b9caf1c6b3d96819ac67
Raveena Liyanage <144007393+raveenaliyanage@users.noreply.github.com>
2023-11-12 11:49:12 +0530

    create InputHeader component

---
+++ src/components/InputHeader.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import CustomIcon from './CustomIcon';
