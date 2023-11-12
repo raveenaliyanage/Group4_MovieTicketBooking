3ddab949c909293bf2226c3141707a286a3e2d39
Raveena Liyanage <144007393+raveenaliyanage@users.noreply.github.com>
2023-11-12 14:22:08 +0530

    try movies

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useEffect,useState} from 'react';
 import{
     Text, 
     View, 
@@ -11,14 +11,78 @@ import{
 } from 'react-native';
 import { COLORS,SPACING } from '../theme/theme';
 import { upcomingMovies,nowPlayingMovies,popularMovies } from '../api/apicalls';
+import InputHeader from '../components/InputHeader';
 
 const {width,height}=Dimensions.get('window');
+
+const getNowPlayingMoviesList = async () => {
+    try {
+        let response = await fetch(nowPlayingMovies);
+        let json = await response.json();
+        return json;
+      } catch (error) {
+        console.error(
+          ' Something went wrong in getNowPlayingMoviesList Function',
+          error,
+        );
+      }
+    };
+
+    const getUpcomingMoviesList = async () => {
+        try {
+          let response = await fetch(upcomingMovies);
+          let json = await response.json();
+          return json;
+        } catch (error) {
+          console.error(
+            ' Something went wrong in getUpcomingMoviesList Function',
+            error,
+          );
+        }
+      };
+      
+      const getPopularMoviesList = async () => {
+        try {
+          let response = await fetch(popularMovies);
+          let json = await response.json();
+          return json;
+        } catch (error) {
+          console.error(
+            ' Something went wrong in getPopularMoviesList Function',
+            error,
+          );
+        }
+      };
+
+      
+
+
 
 const HomeScreen =({navigation}:any)=>{
 const [nowPlayingMoviesList, setNowPlayingMoviesList]=useState<any>(undefined);
 const [popularMoviesList, setPopularMoviesList] = useState<any>(undefined);
 const [upcomingMoviesList, setUpcomingMoviesList] = useState<any>(undefined);
 
+useEffect(() => {
+    (async () => {
+      let tempNowPlaying = await getNowPlayingMoviesList();
+      setNowPlayingMoviesList({...tempNowPlaying});
+
+      let tempPopular = await getPopularMoviesList();
+      setPopularMoviesList({...tempPopular});
+
+
+      let tempUpcoming = await getUpcomingMoviesList();
+      setUpcomingMoviesList({...tempUpcoming});
+    })();
+},[]);
+
+console.log(nowPlayingMoviesList,popularMoviesList,upcomingMoviesList);
+
+
+const searchMoviesFunction=()=>{
+    navigation.navigate('Search');
+}
 if (
     nowPlayingMoviesList == undefined &&
     nowPlayingMoviesList == null &&
@@ -27,22 +91,37 @@ if (
     upcomingMoviesList == undefined &&
     upcomingMoviesList == null
   ) {
-    return  <ScrollView style={styles.container}
-        contentContainerStyle={styles.scrollViewContainer}>
+    return  (
+    <ScrollView 
+    style={styles.container}
+    bounces={false}
+    contentContainerStyle={styles.scrollViewContainer}>
         <StatusBar hidden/>
         <View style={styles.InputHeaderContainer}>
+        <InputHeader searchFunction={searchMoviesFunction} />
         </View>
         <View style={styles.loadingContainer}>
           <ActivityIndicator size={'large'} color={COLORS.Orange} />
         </View>
-    </ScrollView>;
+    </ScrollView>
+    );
+    
   }
 
 
 
 
     return(
-        <View style={styles.container}></View>
+        <ScrollView 
+    style={styles.container}
+    bounces={false}
+    contentContainerStyle={styles.scrollViewContainer}>
+        <StatusBar hidden/>
+        <View style={styles.InputHeaderContainer}>
+        <InputHeader searchFunction={searchMoviesFunction} />
+        
+        </View>
+    </ScrollView>
     );
 };
 
