ac2e3fca4b69956c6aa2e6c621b87ce03bccc8e3
Raveena Liyanage <144007393+raveenaliyanage@users.noreply.github.com>
2023-11-13 17:13:57 +0530

    finish Home Screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,96 +1,96 @@
-import React,{useEffect,useState} from 'react';
-import{
-    Text, 
-    View, 
-    StyleSheet,
-    Dimensions,
-    TouchableOpacity,
-    ActivityIndicator,
-    ScrollView,
-    StatusBar,
-    FlatList,
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  ActivityIndicator,
+  ScrollView,
+  StatusBar,
+  FlatList,
 } from 'react-native';
-import { COLORS,SPACING } from '../theme/theme';
-import { upcomingMovies,nowPlayingMovies,popularMovies, baseImagePath } from '../api/apicalls';
+import {COLORS, SPACING} from '../theme/theme';
+import {
+  upcomingMovies,
+  nowPlayingMovies,
+  popularMovies,
+  baseImagePath,
+} from '../api/apicalls';
 import InputHeader from '../components/InputHeader';
 import CategoryHeader from '../components/CategoryHeader';
 import SubMovieCard from '../components/SubMovieCard';
 import MovieCard from '../components/MovieCard';
 
-const {width,height}=Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
 const getNowPlayingMoviesList = async () => {
-    try {
-        let response = await fetch(nowPlayingMovies);
-        let json = await response.json();
-        return json;
-      } catch (error) {
-        console.error(
-          ' Something went wrong in getNowPlayingMoviesList Function',
-          error,
-        );
-      }
-    };
+  try {
+    let response = await fetch(nowPlayingMovies);
+    let json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(
+      ' Something went wrong in getNowPlayingMoviesList Function',
+      error,
+    );
+  }
+};
 
-    const getUpcomingMoviesList = async () => {
-        try {
-          let response = await fetch(upcomingMovies);
-          let json = await response.json();
-          return json;
-        } catch (error) {
-          console.error(
-            ' Something went wrong in getUpcomingMoviesList Function',
-            error,
-          );
-        }
-      };
-      
-      const getPopularMoviesList = async () => {
-        try {
-          let response = await fetch(popularMovies);
-          let json = await response.json();
-          return json;
-        } catch (error) {
-          console.error(
-            ' Something went wrong in getPopularMoviesList Function',
-            error,
-          );
-        }
-      };
+const getUpcomingMoviesList = async () => {
+  try {
+    let response = await fetch(upcomingMovies);
+    let json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(
+      ' Something went wrong in getUpcomingMoviesList Function',
+      error,
+    );
+  }
+};
 
-      
+const getPopularMoviesList = async () => {
+  try {
+    let response = await fetch(popularMovies);
+    let json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(
+      ' Something went wrong in getPopularMoviesList Function',
+      error,
+    );
+  }
+};
 
+const HomeScreen = ({navigation}: any) => {
+  const [nowPlayingMoviesList, setNowPlayingMoviesList] =
+    useState<any>(undefined);
+  const [popularMoviesList, setPopularMoviesList] = useState<any>(undefined);
+  const [upcomingMoviesList, setUpcomingMoviesList] = useState<any>(undefined);
 
-
-const HomeScreen =({navigation}:any)=>{
-const [nowPlayingMoviesList, setNowPlayingMoviesList]=useState<any>(undefined);
-const [popularMoviesList, setPopularMoviesList] = useState<any>(undefined);
-const [upcomingMoviesList, setUpcomingMoviesList] = useState<any>(undefined);
-
-useEffect(() => {
+  useEffect(() => {
     (async () => {
       let tempNowPlaying = await getNowPlayingMoviesList();
-      setNowPlayingMoviesList(tempNowPlaying.results);
+      setNowPlayingMoviesList([
+        {id: 'dummy1'},
+        ...tempNowPlaying.results,
+        {id: 'dummy2'},
+      ]);
 
       let tempPopular = await getPopularMoviesList();
       setPopularMoviesList(tempPopular.results);
 
-
       let tempUpcoming = await getUpcomingMoviesList();
       setUpcomingMoviesList(tempUpcoming.results);
     })();
-},[]);
+  }, []);
 
-
-
-
-const searchMoviesFunction=()=>{
+  const searchMoviesFunction = () => {
     navigation.navigate('Search');
-};
+  };
 
-
-
-if (
+  if (
     nowPlayingMoviesList == undefined &&
     nowPlayingMoviesList == null &&
     popularMoviesList == undefined &&
@@ -209,28 +209,26 @@ if (
   );
 };
 
-const styles=StyleSheet.create({
-    container:{
-        display:'flex',
-        backgroundColor:COLORS.Black,
-    },
-    scrollViewContainer: {
-        flex: 1,
-    },
-    loadingContainer: {
-        flex: 1,
-        alignSelf: 'center',
-        justifyContent: 'center',
-    },
-    InputHeaderContainer: {
-        marginHorizontal: SPACING.space_36,
-        marginTop: SPACING.space_28,
-    },
-    containerGap36: {
-        gap: SPACING.space_36,
-    },
-
-
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    backgroundColor: COLORS.Black,
+  },
+  scrollViewContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  InputHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_28,
+  },
+  containerGap36: {
+    gap: SPACING.space_36,
+  },
 });
 
 export default HomeScreen;
